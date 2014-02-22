744a4209ad9a0650fe576179b097f597c8974ad4
Cameron Lakenen <lakenen@box.com>
2014-02-22 12:54:30 -0800

    Add inventory and position saving/restoring, death and connection handlers, etc

--- maze.lua
+++ maze.lua
@@ -22,13 +22,10 @@ function PLUGIN:Init()
     self.LogFile = util.GetDatafile('maze_log')
     local x = self.LogFile:GetText()
     if (x ~= '') then
-        self.Log = self.LogFile:GetText()
-        self.Log = 'SERVER RESTARTED.'
-        self:LogSave()
+        self:Log('SERVER RESTARTED.')
     else
         print('Maze :: No log file found. Creating new log file...')
-        self.Log = 'Log created!'
-        self:LogSave()
+        self:Log('Log created!')
         print('Maze :: Log file created!')
     end
 
@@ -36,6 +33,55 @@ function PLUGIN:Init()
     self:AddChatCommand('maze', self.HandleMazeCommand)
 
     print('Maze :: Plugin loaded')
+end
+
+function PLUGIN:OnKilled(takedamage, damage)
+    self:HandlePlayerKilled(takedamage)
+end
+
+function PLUGIN:HandlePlayerKilled(takedamage)
+    local victim = takedamage:GetComponent('HumanController')
+    if (victim) then
+        local netplayer = victim.networkViewOwner
+        if (netplayer) then
+            local netuser = rust.NetUserFromNetPlayer(netplayer)
+            if (netuser) then
+                if (self:UserIsInMaze(netuser)) then
+                    self:Log('User died in maze')
+                    rust.Notice(netuser, 'You died! Select "RESPAWN" to respawn in maze or "AT A CAMP" to exit!')
+                end
+            end
+        end
+    end
+end
+
+function PLUGIN:OnSpawnPlayer(playerclient, usecamp, avatar)
+    timer.NextFrame(function () self:HandlePlayerSpawn(playerclient, usecamp) end)
+end
+
+function PLUGIN:HandlePlayerSpawn(playerclient, usecamp)
+    local netuser = playerclient.netUser
+    if (self:UserIsInMaze(netuser)) then
+        self:Log('Player in maze has spawned')
+        if (usecamp) then
+            self:Log('Player selected use camp... removing them from the maze!')
+            timer.NextFrame(function () self:RemoveUserFromMaze(netuser, usecamp) end)
+            return
+        end
+        self:Log('Sending player to a spawn location')
+        self:SendUserToMaze(netuser)
+    end
+end
+
+function PLUGIN:OnUserConnect(netuser)
+    timer.NextFrame(function() self:HandleUserConnect(netuser) end)
+end
+
+function PLUGIN:HandleUserConnect(netuser)
+    if (self:UserIsInMaze(netuser)) then
+        self:Log('Player is in maze... sending to a spawn location')
+        self:SendUserToMaze(netuser)
+    end
 end
 
 function PLUGIN:HandleMazeCommand(netuser, cmd, args)
@@ -49,13 +95,13 @@ function PLUGIN:HandleMazeCommand(netuser, cmd, args)
     local canAdmin = netuser:CanAdmin()
 
     if (not args[1]) then
-        self:GoToMaze(netuser)
+        self:SendUserToMaze(netuser)
         return
-    elseif (args[1] == 'start') then
-        self:GoToMaze(netuser)
+    elseif (args[1] == 'enter') then
+        self:SendUserToMaze(netuser)
         return
-    elseif (args[1] == 'quit') then
-        -- todo: implement quit
+    elseif (args[1] == 'exit') then
+        self:RemoveUserFromMaze(netuser)
         return
     elseif (args[1] == 'add') then
         if (not canAdmin) then
@@ -150,9 +196,9 @@ function PLUGIN:LoadDefaultConfig()
     end
 end
 
-function PLUGIN:LogSave()
+function PLUGIN:Log(str)
     local templog = self.LogFile:GetText()
-    self.LogFile:SetText(tostring(templog) .. '\n' .. tostring(dateTime()) .. ': ' .. tostring(self.Log)) -- !!! Find better solution for this. !!!
+    self.LogFile:SetText(tostring(templog) .. '\n' .. tostring(dateTime()) .. ': ' .. tostring(str)) -- !!! Find better solution for this. !!!
     self.LogFile:Save()
 end
 
@@ -167,22 +213,24 @@ function PLUGIN:LoadMazeData()
     local txt = self.MazeDataFile:GetText()
     if (txt ~= '') then
         local MazeDataTmp = json.decode(txt)
-        if (not (MazeDataTmp)) then
+        if (MazeDataTmp) then
+            self.MazeData = MazeDataTmp
+            print ('Maze :: Data file loaded.')
+            return;
+        else
             print ('Maze :: Data file is corrupted!')
             --would be good to check for corruption and load backup file
-            return false
+            -- just fall back to loading defaults...
         end
-        self.MazeData = MazeDataTmp
-        print ('Maze :: Data file loaded.')
-        return true
-    else
-        print ('Maze :: Data file is empty! Setting defaults.')
-        self.MazeData = {}
-        self.MazeData['meta'] = {}
-        self.MazeData['meta']['VERSION'] = '1.0'
-
-        self:SaveMazeData()
     end
+
+    print ('Maze :: Data file is empty/corrupted! Setting defaults.')
+    self.MazeData = {}
+    self.MazeData.meta = {}
+    self.MazeData.users = {}
+    self.MazeData.spawnLocations = {}
+
+    self:SaveMazeData()
 end
 
 function PLUGIN:AddSpawnLocation(netuser, name)
@@ -204,8 +252,7 @@ function PLUGIN:AddSpawnLocation(netuser, name)
         z = coords.z
     }
     self:SaveMazeData()
-    self.Log =  'Created maze spawn point "' .. name .. '"'
-    self:LogSave()
+    self:Log('Created maze spawn point "' .. name .. '"')
 
     rust.Notice(netuser, 'Spawn location set!')
 end
@@ -215,8 +262,7 @@ function PLUGIN:DeleteSpawnLocation(netuser, name)
         if (name == 'all') then
             self.MazeData.spawnLocations = {}
             self:SaveMazeData();
-            self.Log =  'Removed all maze spawn points'
-            self:LogSave()
+            self:Log('Removed all maze spawn points')
             rust.Notice(netuser, 'All spawn locations removed!')
             return
         end
@@ -226,8 +272,7 @@ function PLUGIN:DeleteSpawnLocation(netuser, name)
 
     self.MazeData.spawnLocations[name] = nil
     self:SaveMazeData()
-    self.Log =  'Removed maze spawn point "' .. name .. '"'
-    self:LogSave()
+    self:Log('Removed maze spawn point "' .. name .. '"')
 
     rust.Notice(netuser, 'Spawn location removed!')
 end
@@ -254,10 +299,31 @@ function getRandomFromTable(t)
     return t[keys[math.random(count - 1)]]
 end
 
-function PLUGIN:GoToMaze(netuser)
+function PLUGIN:SendUserToMaze(netuser)
     local spawnLocations = self.MazeData.spawnLocations
     local playerCoords = netuser.playerClient.lastKnownPosition
     local spawnCoords = getRandomFromTable(spawnLocations)
+    local inv = rust.GetInventory(netuser)
+    local netuserId = rust.GetUserID(netuser)
+
+    if (not self:UserIsInMaze(netuser)) then
+        self:Log('Adding new user to the maze! Saving previous inventory and location...')
+        if (not self.MazeData.users) then
+            -- first user!
+            self.MazeData.users = {}
+        end
+
+        -- save user's position and items before entering maze for the first time
+        self.MazeData.users[netuserId] = {
+            inventory = self:GetAllInventoryItems(inv),
+            location = {
+                x = playerCoords.x,
+                y = playerCoords.y,
+                z = playerCoords.z
+            }
+        }
+        self:SaveMazeData();
+    end
 
     playerCoords.x = spawnCoords.x
     playerCoords.y = spawnCoords.y + self.MazeConfig.yOffset
@@ -267,32 +333,95 @@ function PLUGIN:GoToMaze(netuser)
     rust.ServerManagement():TeleportPlayer(netuser.playerClient.netPlayer, playerCoords)
 
     -- clear their inventory, and give them a few things...
-    local inv = rust.GetInventory(netuser)
     inv:Clear()
-    for i, item in pairs(self.MazeConfig.items) do
-        self:GiveItem(netuser, item.name, item.amount)
+    for name, amount in pairs(self.MazeConfig.items) do
+        self:GiveItem(inv, name, amount)
     end
 
-    -- todo: make sure they spawn at a different starting point if they die
-
-    rust.Notice(netuser, 'You are now in the maze. Good luck!')
+    rust.Notice(netuser, 'You are now in the maze. Good luck! Type "/maze exit" to leave.')
 end
 
-function PLUGIN:GiveItem(netuser, name, amount)
+function PLUGIN:RemoveUserFromMaze(netuser, noteleport)
+    if (not self:UserIsInMaze(netuser)) then
+        self:Log('Attempted to remove a user that is not in the maze!')
+        return
+    end
+
+    self:Log('Removing user from the maze! Restoring previous inventory and location...')
+
+    local netuserId = rust.GetUserID(netuser)
+    local userData = self.MazeData.users[netuserId]
+    local playerCoords = netuser.playerClient.lastKnownPosition
+    local inv  = rust.GetInventory(netuser)
+    playerCoords.x = userData.location.x
+    playerCoords.y = userData.location.y
+    playerCoords.z = userData.location.z
+
+    -- clear out their maze items and restore their original items
+    -- todo: figure out how to recall item positions in inv
+    inv:Clear()
+    for name, amount in pairs(userData.inventory) do
+        self:GiveItem(inv, name, amount)
+    end
+
+    -- remove the user's entry from the MazeData
+    self.MazeData.users[netuserId] = nil
+    self:SaveMazeData();
+
+    if (not noteleport) then
+        rust.ServerManagement():TeleportPlayer(netuser.playerClient.netPlayer, playerCoords)
+    end
+    rust.Notice(netuser, 'You are now back where you started. Hope you had fun!')
+end
+
+function PLUGIN:UserIsInMaze(netuser)
+    local netuserId = rust.GetUserID(netuser)
+    if self.MazeData.users and self.MazeData.users[netuserId] then
+        return true
+    end
+    return false
+end
+
+function PLUGIN:GiveItem(inventory, name, amount)
+    self:Log('Giving user ' .. name .. ' x' .. amount)
     local item = tostring(name)
     local datablock = rust.GetDatablockByName(item)
     if (not datablock) then
-        self.Log =  'Bad item name "' .. name .. '"'
-        self:LogSave()
+        self:Log('Bad item name "' .. name .. '"')
         return
     end
     amount = math.ceil(tonumber(amount))
     if (amount < 1) then
-        self.Log =  'Bad item amount "' .. amount .. '"'
-        self:LogSave()
+        self:Log('Bad item amount "' .. amount .. '"')
         return
     end
-    local inv = rust.GetInventory(netuser)
-    inv:AddItemAmount(datablock, amount)
+    inventory:AddItemAmount(datablock, amount)
 end
 
+-- borrowed from invviewer (thanks to CareX)
+local unstackable = {"M4", "9mm Pistol", "Shotgun", "P250", "MP5A4", "Pipe Shotgun", "Bolt Action Rifle" , "Revolver", "HandCannon", "Research Kit 1", "Torch" }
+function table.containsval(t,cv) for _, v in ipairs(t) do  if v == cv then return true  end  end return nil end -- return true if value is in said table.
+function PLUGIN:GetAllInventoryItems(inv)
+    local inventory = {}
+    local i = 0
+    while (i <= 39) do
+        local b, item = inv:GetItem(i)
+        if (b) then
+            local s = tostring(item)
+            local x = string.find(s, "%(on", 2) -2
+            local itemname = string.sub(s, 2, x)
+            local isUnstackable = table.containsval(unstackable, itemname)
+            local amount = 1
+            if (not isUnstackable) then
+                amount = item.uses
+            end
+            if (inventory[itemname]) then
+                inventory[itemname] = inventory[itemname] + amount
+            else
+                inventory[itemname] = amount
+            end
+        end
+        i = i + 1
+    end
+    return inventory
+end
